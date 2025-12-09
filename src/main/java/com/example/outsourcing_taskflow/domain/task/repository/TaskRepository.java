fca4b55b9fe260395d0b3c18d047b04e4bded4a0
BullGombo <ormaju15123647@gmail.com>
2025-12-09 17:43:07 +0900

    ✨ feat(Task - Entity, Repository, Enum): Task의 Entity, Repository, Enum
    #Task의 Entity, Repository, Enum 파일을 생성함
    - Task.java
    - TaskRepository
    - TaskStatusEnum
    - TaskPriorityEnum

--- src/main/java/com/example/outsourcing_taskflow/domain/task/repository/TaskRepository.java
+++ src/main/java/com/example/outsourcing_taskflow/domain/task/repository/TaskRepository.java
@@ -4,4 +4,5 @@ import com.example.outsourcing_taskflow.common.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    //
 }
