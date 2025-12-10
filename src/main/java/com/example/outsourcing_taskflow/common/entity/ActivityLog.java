0587da6e4a53775b643e7b230d51faad6bcef6f5
Michael-930813 <michael.930813@gmail.com>
2025-12-10 09:08:29 +0900

    ✨ feat: ActivityLog 기능 구현
    1. ActivityLog Entity Class 추가
    2. ActivityType Enum 추가

---
+++ src/main/java/com/example/outsourcing_taskflow/common/entity/ActivityLog.java
@@ -0,0 +1,41 @@
+package com.example.outsourcing_taskflow.common.entity;
+
+import com.example.outsourcing_taskflow.common.enums.ActivityType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "activitylogs")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ActivityLog {
+// - Properties
+    // - 고유ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    // - 활동 타입
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ActivityType type;
+    // - 사용자
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+    // - 작업 PK
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "task_id")
+    private Task task;
+    // - 로그 내용
+    @Column(nullable = false, length = 255)
+    private String description;
+    // - 생성일
+    @CreatedDate
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+}
