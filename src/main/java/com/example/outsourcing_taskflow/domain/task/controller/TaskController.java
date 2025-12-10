6f52c06e76a4f248ee2d40dfea488a6efab358cc
BullGombo <ormaju15123647@gmail.com>
2025-12-10 18:03:28 +0900

    ✨ feat: 작업 목록 조회 (임시)
    푸시 안함

--- src/main/java/com/example/outsourcing_taskflow/domain/task/controller/TaskController.java
+++ src/main/java/com/example/outsourcing_taskflow/domain/task/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.example.outsourcing_taskflow.domain.task.controller;
 
 import com.example.outsourcing_taskflow.common.response.ApiResponse;
+import com.example.outsourcing_taskflow.common.response.PageResponse;
 import com.example.outsourcing_taskflow.domain.task.dto.request.CreateTaskRequest;
 import com.example.outsourcing_taskflow.domain.task.dto.response.CreateTaskResponse;
 import com.example.outsourcing_taskflow.domain.task.dto.response.GetTaskResponse;
@@ -23,11 +24,24 @@ public class TaskController {
         return ApiResponse.success("작업이 생성되었습니다.", response);
     }
 
-    // 작성 상세 조회
+    // 작업 상세 조회
     @GetMapping("/{id}")
     public ApiResponse<GetTaskResponse> getTask(@Valid @PathVariable Long id) {
         GetTaskResponse response = taskService.getTask(id);
         return ApiResponse.success("작업 조회 성공", response);
     }
+
+    // 작업 목록 조회
+    @GetMapping
+    public PageResponse<GetTaskResponse> getAllTask(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String status,
+            @RequestParam(required = false) String search,
+            @RequestParam(required = false) Long assigneeId
+    ) {
+        return taskService.getAllTasks(page, size, status, search, assigneeId);
+    }
+
 
 }
--- src/main/java/com/example/outsourcing_taskflow/domain/task/service/TaskService.java
+++ src/main/java/com/example/outsourcing_taskflow/domain/task/service/TaskService.java
@@ -5,6 +5,7 @@ import com.example.outsourcing_taskflow.common.entity.User;
 import com.example.outsourcing_taskflow.common.enums.ErrorMessage;
 import com.example.outsourcing_taskflow.common.enums.TaskStatusEnum;
 import com.example.outsourcing_taskflow.common.exception.CustomException;
+import com.example.outsourcing_taskflow.common.response.PageResponse;
 import com.example.outsourcing_taskflow.domain.task.dto.request.CreateTaskRequest;
 import com.example.outsourcing_taskflow.domain.task.dto.response.CreateTaskResponse;
 import com.example.outsourcing_taskflow.domain.task.dto.response.GetTaskResponse;
@@ -13,6 +14,9 @@ import com.example.outsourcing_taskflow.domain.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -55,6 +59,43 @@ public class TaskService {
                 .orElseThrow(() -> new CustomException(ErrorMessage.NOT_FOUND_TASK));
 
         return GetTaskResponse.from(task);
+    }
+
+    public PageResponse<GetTaskResponse> getAllTasks(int page, int size, String status, String search, Long assigneeId) {
+
+        // page, size 유효성 검사
+        if (page < 0 || size <= 0) {
+            // 지금 당장 페이징 관련 커스텀 예외가 없으니 임시로..
+            throw new IllegalArgumentException("페이지가 '0'미만이거나 사이즈가 '0'이하일 수 없습니다.");
+        }
+
+        // pageable : 페이징 정보를 담고 있는 객체
+        Pageable pageable = PageRequest.of(page, size);
+        // JPA Specification : DB 쿼리의 조건을 간단히 Spec으로 작성하여 날릴 수 있게 해줌, Repository인터페이스에 선언해서 씀
+        //Specification<Task> spec = Specification.where(null); 다른 좋은 방법은 없나..
+        
+        // 작업 상태 유효성 검사
+        if (status != null) {
+            try {
+                TaskStatusEnum statusEnum = TaskStatusEnum.valueOf(status.toUpperCase());   // toUpperCase() 사실상 불필요..?
+
+            } catch (IllegalArgumentException e) {
+                throw new CustomException(ErrorMessage.NOT_CORRECT_TASK_STATUS);
+            }
+        }
+
+        // 검색어
+
+
+        // 담당자
+        
+        
+        // 조회
+        
+        
+        // 반환
+        return PageResponse.success("작업 목록 조회 성공", responsePage);
+
     }
 
 }
