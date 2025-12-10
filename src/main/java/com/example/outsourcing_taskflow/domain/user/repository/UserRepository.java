70f03cb4860d11c3769da6713ced9be2b4826d62
remnantcjy <remnantcjy@gmail.com>
2025-12-10 16:07:42 +0900

    ✨ feat: 2. 사용자 - 회원가입 구현 완료
    - UserController @PostMapping("/api/users") 추가
    - User 생성자 구현
    - UserDto 생성
    - UserService 회원가입 관련 로직 구현
    - UserRepository 기능 추가
    - CreateUserRequest, CreateUserResponse 생성
    - ErrorMessage 예외 메시지: EXIST_NAME 추가
    - build.gradle 스프링 시큐리티 종속성 주석 처리

--- src/main/java/com/example/outsourcing_taskflow/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/outsourcing_taskflow/domain/user/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.example.outsourcing_taskflow.domain.user.repository;
 
 import com.example.outsourcing_taskflow.common.entity.User;
+import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    boolean existsByUserName(String userName);
+
+    boolean existsByEmail(@Email(message = "올바른 이메일 형식이 아닙니다.") String email);
 }
