80dedff8751b74c213fd2947fc60973f3c560d5a
remnantcjy <remnantcjy@gmail.com>
2025-12-11 18:00:54 +0900

    🐛 fix: IsDeleted 충돌 해결

--- src/main/java/com/example/outsourcing_taskflow/domain/team/repository/TeamRepository.java
+++ src/main/java/com/example/outsourcing_taskflow/domain/team/repository/TeamRepository.java
@@ -1,18 +1,18 @@
 package com.example.outsourcing_taskflow.domain.team.repository;
 
 import com.example.outsourcing_taskflow.common.entity.Team;
+import com.example.outsourcing_taskflow.common.enums.IsDeleted;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
     // 팀 이름으로 조회하는 메서드
     Optional<Team> findByTeamName(String teamName);
-    Optional<Team> findByIdAndIsDeleted(Long id, IsDeleted isDeleted);
-    List<Team> findAllByIsDeleted(IsDeleted isDeleted);
 
     // - Search By Keyword
     @Query("""
